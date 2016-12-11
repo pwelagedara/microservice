969023429cbd57c9459502d7afb90eacf7c27cf0
Pubudu <pubuduwelagedara@gmail.com>
2016-12-11 09:42:20 +0530

    test

--- src/main/java/org/pongmatcher/Application.java
+++ src/main/java/org/pongmatcher/Application.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
+    	// some comment
         SpringApplication.run(Application.class, args);
     }
 }
